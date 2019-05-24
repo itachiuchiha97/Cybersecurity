78e70f7973a32ca600f2662f5ae6aaf07e78805b
Sahil <sahil.singh.bhatia@gmail.com>
2019-05-24 17:18:35 +0530

    adding views to app.js

--- Capella Application/Node-passport-login/routes/index.js
+++ Capella Application/Node-passport-login/routes/index.js
@@ -3,7 +3,10 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/',(req, res) => res.send('Welcome'))
+// router.get('/',(req, res) => res.send('Welcome'))
 //seding responsed when routes are requested
+
+router.get('/',(req, res) => res.render('welcome'))
+//view that we wanna render
 
 module.exports = router
