bd29f08fdc398c7e82955dd8399e5b8a7151b78e
Sahil <sahil.singh.bhatia@gmail.com>
2019-05-24 17:49:18 +0530

    addding bootstrap registration view logic and adding it to our routes

--- Capella Application/Node-passport-login/routes/users.js
+++ Capella Application/Node-passport-login/routes/users.js
@@ -3,10 +3,16 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/login',(req, res) => res.send('Login Page'))
-//seding responsed when routes are 
+// router.get('/login',(req, res) => res.send('Login Page'))
+// //seding responsed when routes are  
 
-router.get('/register',(req, res) => res.send('Register Page'))
+// router.get('/register',(req, res) => res.send('Register Page'))
+
+
+router.get('/login',(req, res) => res.render('login'))
+//to render login and register pages
+router.get('/register',(req, res) => res.render('register'))
+
 
 
 module.exports = router
